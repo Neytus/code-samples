f847fb8d2d4d0caad8a2abe3b1ca2225ff6c8125
JurajB <JurajB@kentico.com>
2019-02-14 08:15:37 +0100

    Update skrtic.py

--- python/skrtic.py
+++ python/skrtic.py
@@ -1,3 +1,29 @@
 # DocSection: single_skrtic
-Python - skrtic
+Python - skrtic velikan
 # EndDocSection
+
+# DocSection: single_velhad
+Python - velhad
+
+
+# EndDocSection
+hghgfh
+
+ghf
+gf
+hh
+
+# DocSection: multiple_REPTILES
+
+hadyhadyhady
+
+# EndDocSection
+
+sssssssssssssss
+
+dsgfdffd
+fd
+s
+fds
+fds
+f
