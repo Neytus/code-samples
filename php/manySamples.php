a333d189d4a700f39df8febf730051b21cd8b931
Juraj Bielik <jurajb@kentico.com>
2019-03-21 14:42:53 +0100

    Add many samples

---
+++ php/manySamples.php
@@ -0,0 +1,377 @@
+# DocSection: php_1
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_2
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_3
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_4
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_5
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_6
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_7
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_8
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_9
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_10
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_11
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_12
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_13
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_14
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_15
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_16
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_17
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_18
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_30
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_31
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_32
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_33
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_34
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_35
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_36
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_37
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_38
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php310
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php31
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php312
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php313
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php314
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php315
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php316
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php317
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php318
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_105
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_115
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_125
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_135
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_145
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_155
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_165
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_175
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_185
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_10X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_11X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_12X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_13X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_14X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_15X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_16X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_17X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_18X
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_10dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_11dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_12dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_13dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_14dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_15dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_16dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_17dDoIT
+
+sdaasdasd
+
+# EndDocSection
+
+# DocSection: php_18dDoIT
+
+sdaasdasd
+
+# EndDocSection
