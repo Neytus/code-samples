6a1cee3401c8a9f7054c05ca67e4267eee7d703f
Juraj Bielik <jurajb@kentico.com>
2019-04-18 15:38:21 +0200

    asjdhsah

--- android/javaFile.java
+++ android/javaFile.java
@@ -11,16 +11,6 @@ sd
 
 fdfsdsf
 
-
-// DocSection: content_unpublishing_v3
-
-JavaRX supercode basic
-
-
-ZMEMENY CODE SAMPLE LETS SEE
-
-// EndDocSection
-
 // DocSection: content_unpublishing_v2
 
 JavaRX supercode v2 - after PR
