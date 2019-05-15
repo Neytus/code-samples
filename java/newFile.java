edd14ad52cfb8d4819c947ce893295c1af4aa72d
Juraj Bielik <jurajb@kentico.com>
2019-05-15 09:46:53 +0200

    delete new files

--- java/newFile.java
+++
@@ -1,5 +0,0 @@
-// DocSection: new_file_sample
-
-lets see how you handle this
-
-// EndDocSection
--- js/newFile.js
+++
@@ -1,6 +0,0 @@
-// DocSection: new_file_sample
-
-lets see how you handle this changed code sample
-
-// EndDocSection
-
