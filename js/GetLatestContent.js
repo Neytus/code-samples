5fccdaef76646c5bb8f515dc720453a81538649d
Juraj Bielik <JurajB@kentico.com>
2019-06-04 08:39:28 +0200

    Test commit

--- js/GetLatestContent.js
+++ js/GetLatestContent.js
@@ -1,4 +1,4 @@
-// DocSection: test_codename_usz36vlxce
+// DocSection: test_codename_uc1s3g6s12
 const KenticoCloud = require('kentico-cloud-delivery');
 
 // Create strongly typed models according to https://developer.kenticocloud.com/docs/strongly-typed-models
@@ -7,7 +7,7 @@ class Article extends KenticoCloud.ContentItem222 {
         super();
     }
 }
-const deliveryClient = new javascript code sample_3X75LI02aQ({
+const deliveryClient = new javascript code sample_ClqNB4XMl5({
     projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
     typeResolvers: [
         new KenticoCloud.TypeResolver('article', () => new Article())
