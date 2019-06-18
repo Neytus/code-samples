2a5f43a90bd08f8e6b6515b813fdb72c6a17a0a3
Juraj Bielik <JurajB@kentico.com>
2019-06-18 09:04:29 +0200

    Update someFile.java

--- android/someFile.java
+++ android/someFile.java
@@ -1,18 +1,43 @@
 
-// DocSection: kukukukukkukukukukukukukukuk
-
-ahahahaha
-
-// EndDocSection
-
 dsgadyifyasyfs
 
 dgf
 df
 s
+// DocSection: abcdefgh
+
+wowowowowowowow dnsdjsdfjjdf
+hg
+h
+df
+fg
+dfg
+d
+fg
+
+f
+gdsgf
+g
+s
+hjsgf
+jfgf
+jj
+gh
+jghj
+g
+gj
+
+// EndDocSection
+
 gd
 fg
 s
 d;;g;agd;agdfjgsfgusfg{{}YUYHUD
 FYdfs
+
+// DocSection: novy_vztvoreny
+
+chichichichi
+
+// EndDocSection
 
